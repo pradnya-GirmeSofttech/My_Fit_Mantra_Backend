43616ce5338b440b9e63b00eada4f03b9188f582
pradnya <chavanpradnya1988@gmail.com>
2024-01-04 12:15:43 +0530

    "Bug fixing"

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import HabitRouter from "./router/Habit.js";
 import cors from "cors";
 
 export const app = express();
-app.use("/api/v1", HabitRouter);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", HabitRouter);
